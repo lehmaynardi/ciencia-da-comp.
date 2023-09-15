1a0c274ea0b5d97c0d0b840a9b47dd492a7d663f
lehmaynardi <132676542+lehmaynardi@users.noreply.github.com>
2023-09-15 12:09:00 -0300

    Add files via upload

---
+++ ciencia da comp Lethicia/teste.py
@@ -0,0 +1,14 @@
+# variáveis
+#print ("olá mundo!")
+#idade = 18 
+#print (idade)
+velocidade__Maxima = 200
+#print(velocidade__Maxima)
+frase_De_Boas_Vindas = "olá, seja bem-vindo(a)!" #String
+print(frase_De_Boas_Vindas)
+#posso entrar em_Contato = False  #Booleanos
+#print(frase_De_Boas_Vindas)
+posso_Entrar_Em_Contato = False #Booleanos
+print(posso_Entrar_Em_Contato)
+
+
